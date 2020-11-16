0d853386e652b78152264c306c5fc49d12c9e444
FaisalNajibAbdullah <faisalnajib23@gmail.com>
2020-11-16 19:25:26 +0700

    Najib 3

--- examples/panas2.py
+++ examples/panas2.py
@@ -8,6 +8,8 @@ import picamera.array
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import griddata
+from firebase import firebase
+import datetime
 
 plt.ion()
 plt.subplots(figsize=(8, 4))
@@ -38,6 +40,14 @@ def map(x, in_min, in_max, out_min, out_max):
 
 displayPixelWidth = width / 30
 displayPixelHeight = height / 30
+
+t = datetime.datetime.now()
+
+try:
+    firebase = firebase.FirebaseApplication('https://termalcam-d9c8e.firebaseio.com/')
+except:
+    print ("Koneksi Gagal")
+
 
 try:
 	sensor = Adafruit_AMG88xx()		
@@ -73,6 +83,7 @@ try:
 		varA = 0.022380841
 		varb1 = -0.00166934
 		varb2 = 1.173835088
+		mxs = 38
 		rums100 = varA + (varb1 * 100) + (varb2 * max_temp) + 2.503336
 		#rums150 = varA + (varb1 * 150) + (varb2 * max_temp) + 3.17472
 		#rums200 = varA + (varb1 * 200) + (varb2 * max_temp) + 3.844105
@@ -83,9 +94,22 @@ try:
 		wajah = face.detectMultiScale(gray, 1.3, 5)
 
 		for (x,y,w,h) in wajah:
-			cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
-			cv2.putText(img, str("%.2f" % rums100) +' Celcius', (x, y - 10),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0,255,0), 2)
+			if rums100 >= mxs:
+				cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+				cv2.putText(img, str("%.2f" % rums100) +' Celcius', (x, y - 10),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255,0,0), 2)
+				try:
+					result = firebase.post('termalcam', {'tingkat':str("Tidak Aman"), 'rTemp':str("%.2f" % rums100), 'aTemp':str(max_temp), 'time':str(t)})
+				except:
+					print ("Koneksi Gagal")
+			else:
+				cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+				cv2.putText(img, str("%.2f" % rums100) +' Celcius', (x, y - 10),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0,255,0), 2)
+				try:
+					result = firebase.post('termalcam', {'tingkat':str("Aman"), 'rTemp':str("%.2f" % rums100), 'aTemp':str(max_temp), 'time':str(t)})
+				except:
+					print ("Koneksi Gagal")
 
 			cv_warna = img[y:y+h, x:x+w]
 			cv_gray = gray[y:y+h, x:x+w]
